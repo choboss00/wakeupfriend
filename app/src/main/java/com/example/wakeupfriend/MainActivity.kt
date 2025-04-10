5950067ae1271aa874d20997a1e15eac71e2ef4c
Jin_00 <anjdal64@gmail.com>
2025-04-10 23:44:09 +0900

    feat : 화면 표시 및 간 이동 구현
    추후 보충 필요(화면 레이아웃 등)

--- app/src/main/java/com/example/wakeupfriend/MainActivity.kt
+++ app/src/main/java/com/example/wakeupfriend/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.example.wakeupfriend
 
+import AlarmListScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import com.example.wakeupfriend.data.datasource.Alarm
+import com.example.wakeupfriend.ui.screen.AlarmMainScreen
 import com.example.wakeupfriend.ui.theme.WakeUpFriendTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Compose UI content here
             WakeUpFriendTheme {
-                // Call your composables here
+                val navController = rememberNavController()
+                var alarms by remember { mutableStateOf(listOf<Alarm>()) }  // 알람 목록 상태
+
+                NavHost(navController = navController, startDestination = "alarm_main") {
+                    composable("alarm_main") {
+                        AlarmMainScreen(navController = navController, onAlarmAdded = { alarm ->
+                            // 알람이 추가되면 알람 목록에 추가
+                            alarms = alarms + alarm
+                            navController.navigate("alarm_list") // 알람 목록 화면으로 이동
+                        })
+                    }
+                    composable("alarm_list") {
+                        AlarmListScreen (alarms = alarms, onAlarmClick = { alarm ->
+                            // 알람을 클릭했을 때의 동작을 여기에 추가
+                        })
+                    }
+                }
             }
         }
     }
